2f483fc2be3b07f5a080e6e4943608fda66fcf17
Sehsyha <valentin.stern@gmail.com>
2021-04-04 22:03:19 +0200

    feat(humidity): add mqtt connector to save samples

---
+++ ports/mqtt/humidity.go
@@ -0,0 +1,33 @@
+package mqtt
+
+import (
+	"fmt"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+const (
+	humidityTopicTemplate = "%s/humidity"
+)
+
+func (s *Server) SubscribeToRoomHumidity(room string) {
+	topic := fmt.Sprintf(humidityTopicTemplate, room)
+
+	token := s.client.Subscribe(topic, 1, s.HumidityHandler)
+	token.Wait()
+
+	s.logging.Debugf("Subscribed to \"%s\" humidity", room)
+}
+
+func (s *Server) HumidityHandler(client mqtt.Client, msg mqtt.Message) {
+	room := getRoomFromMessage(msg)
+
+	humidityValue, err := parseFloatPayload(msg)
+	if err != nil {
+		return
+	}
+
+	s.logging.Debugf("Received humidity sample \"%f\" for room \"%s\"", humidityValue, room)
+	s.service.SaveHumidity(room, time.Now(), humidityValue)
+}
---
+++ ports/mqtt/temperature.go
@@ -0,0 +1,33 @@
+package mqtt
+
+import (
+	"fmt"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+const (
+	temperatureTopicTemplate = "%s/temperature"
+)
+
+func (s *Server) SubscribeToRoomTemperature(room string) {
+	topic := fmt.Sprintf(temperatureTopicTemplate, room)
+
+	token := s.client.Subscribe(topic, 1, s.TemperatureHandler)
+	token.Wait()
+
+	s.logging.Debugf("Subscribed to \"%s\" temperature", room)
+}
+
+func (s *Server) TemperatureHandler(client mqtt.Client, msg mqtt.Message) {
+	room := getRoomFromMessage(msg)
+
+	temperatureValue, err := parseFloatPayload(msg)
+	if err != nil {
+		return
+	}
+
+	s.logging.Debugf("Received temperature sample \"%f\" for room \"%s\"", room)
+	s.service.SaveTemperature(room, time.Now(), temperatureValue)
+}
